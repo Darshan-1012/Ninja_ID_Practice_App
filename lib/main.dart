c525125739fb0d10596cf1b3c81edd26046c571e
Darshan-1012 <bdarshan524@gmail.com>
2025-06-10 11:29:32 +0530

    ninja ID project -- complete

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 }
 
 class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +24,18 @@ class NinjaCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/ninja.jpeg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+            ),
             Text(
               'NAME',
               style: TextStyle(
@@ -30,13 +43,51 @@ class NinjaCard extends StatelessWidget {
                 letterSpacing: 2,
               ),
             ),
+            SizedBox(height: 10),
             Text(
               'John-Doe',
               style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+             Text(
+                'CURRENT NINJA LEVEL',
+                style: TextStyle(
                 color: Colors.grey,
                 letterSpacing: 2,
               ),
-            )
+            ),
+            SizedBox(height: 10),
+            Text(
+                '8',
+                style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30),
+
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[700],
+                ),
+                SizedBox(width: 10,),
+                Text(
+                  'john_doe@ninja1234.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
         ),
